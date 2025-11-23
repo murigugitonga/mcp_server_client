bbd2f977d3666b995e1fdb66118f2e129dd98cd2
adriamdevs <murigucharles3@gmail.com>
2025-11-23 12:06:55 +0300

    pending: fall back to mcp/sdk stable version

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js"
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js"
-
+//import { StreamableHTTPServerTransport } from "@modelcontextprotocol/sdk/server/streamableHttp.js"
+import {z} from "zod"
 const server = new McpServer({
     name : "testServer",
     version : "1.0.0",
@@ -11,6 +12,33 @@ const server = new McpServer({
     }
 })
 
+server.tool("create-user", "Create a new user in the database",
+    {
+    name: z.string()
+    email : z.string(),
+    address : z.string(),
+    phone: z.string
+},
+{
+    title : "Create User",
+    readOnlyHint : false,
+    destructiveHint : false,
+    idempotentHint : false,
+    openWorldHint : true
+}
+, async ()=>{
+   try {
+    
+   } catch (error) {
+    return {
+        content :[
+            {type: "text", text:"failed to save user"}
+        ]
+    }
+   }
+})
+
+// Stdio for transport layer for local application
 
 async function main() {
     const transport = new StdioServerTransport()
