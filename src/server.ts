454325e47d0da6990f9dbec2af40ef9b39d7c699
adriamdevs <murigucharles3@gmail.com>
2026-01-31 20:32:29 +0300

    pull changes & merge

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,9 @@ import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js"
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js"
 //import fs from "node:fs/promises"
 //import { StreamableHTTPServerTransport } from "@modelcontextprotocol/sdk/server/streamableHttp.js"
-import {z} from "zod"
+import {string, z} from "zod"
 
 import fs from "node:fs/promises"
-
-
-
 
 const server = new McpServer({
     name : "firstServer",
@@ -22,7 +19,7 @@ const server = new McpServer({
 // Creating the tool
 server.tool("create-user", "Create a new user in the database",
     {
-    name: z.string(),
+    name : z.string(),
     email : z.string(),
     address : z.string(),
     phone: z.string()
@@ -44,7 +41,6 @@ server.tool("create-user", "Create a new user in the database",
     return {
         content :[
             {type: "text", text:`User${id} created successfully`},
-
         ]
     }
    } catch {
@@ -68,16 +64,16 @@ async function createUser(user:{
      const id = users.length + 1
      users.push({ id, ...user })
 
-     await fs.writeFile("./src/data/users.json", JSON.stringify(users,null, 2))
+     await fs.writeFile("./src/data/users.json", JSON.stringify(users,null, 2)) // append record to JSON source
 
      return id
 }
 
-// Stdio for transport layer for local application
-async function main() {
+// Stdio transport layer for local application
+(async function main() {
     // transport protocol
     const transport = new StdioServerTransport() // local communication
     await server.connect(transport)
-}
+}())
 
-main()
+//main()
